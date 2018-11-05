184b57945c00fc254d68956cd6f98fb6c3814854
Peter Gonda <pipo@pipoline.com>
2018-11-05 21:29:41 +0100

    Add special character option

--- README.md
+++ README.md
@@ -4,6 +4,18 @@ This is a simple password generator written in python.
 
 ## Usage
 
+Basic usage:
+
+```
+usage: pypassword.py [-h] [-s SIZE] [-x]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -s SIZE, --size SIZE  Random string size
+  -x                    When set, use special characters
+```
+
+
 `docker run --rm pipoline/random-password`  
 output:  
 `a8xlHjNEQ1sLFj`
@@ -13,3 +25,10 @@ When you want to change password length just add -s/--size parameter e.g.:
 `docker run --rm pipoline/random-password -s 20`  
 output:  
 `bjMhaBqnw5Y8TvsmNLZV`
+
+Usage with special characters:  
+
+`docker run --rm pipoline/random-password -s 20 -x`  
+output:  
+```O`v5|!XnV#N@[UXtg%Ei```
+
--- pypassword/pypassword.py
+++ pypassword/pypassword.py
@@ -4,7 +4,17 @@ import argparse
 
 
 def password_generator(size=6,
-                       chars=string.ascii_letters + string.digits):
+                       chars=string.ascii_letters + string.digits,
+                       special=False):
+    """
+    Generate password
+    :param size: Size is number of chars
+    :param chars: Charset
+    :param special: Boolean, us special characters or not
+    :return: Random string
+    """
+    if special:
+        chars += string.punctuation
     return ''.join(random.SystemRandom().choice(chars) for _ in range(size))
 
 
@@ -12,9 +22,11 @@ if __name__ == "__main__":
     size = 14
     parser = argparse.ArgumentParser()
     parser.add_argument("-s", "--size", help="Random string size", type=int)
+    parser.add_argument("-x", help="Use special characters", action="store_true")
 
     args = parser.parse_args()
     if args.size:
         size = args.size
 
-    print(password_generator(size=size))
+    print(password_generator(size=size,
+                             special=args.x))
