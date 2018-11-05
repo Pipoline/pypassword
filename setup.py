0bafb9320516396584c3804f3eaf6dd3cf40331a
Peter Gonda <pipo@pipoline.com>
2018-11-05 20:46:13 +0100

    Add pypi files, update gitignore

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pypassword",
+    version="0.0.1",
+    author="Peter Gonda",
+    author_email="peter@pipoline.com",
+    description="Simple password generator module",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Pipoline/random-password",
+    packages=setuptools.find_packages(),
+    license="MIT",
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Topic :: Utilities",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
