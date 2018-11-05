3a6ab699edb7c54de65ec8325663099a615c183f
pipo <pipo@pipoline.com>
2018-11-05 22:23:13 +0100

    Bump version: 0.0.2 → 0.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pypassword",
-    version="0.0.2",
+    version="0.0.3",
     author="Peter Gonda",
     author_email="peter@pipoline.com",
     description="Simple password generator module",
