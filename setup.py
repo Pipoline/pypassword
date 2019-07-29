412112bb32450d58bd05fa9c50811a803db106f0
Peter Gonda <peter@pipoline.com>
2019-07-29 13:48:32 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Simple password generator module",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/Pipoline/random-password",
+    url="https://github.com/Pipoline/pypassword",
     packages=setuptools.find_packages(),
     license="MIT",
     include_package_data=True,
